50e099faad83ebaf3c8645d943c1929a21cd7cda
Praneethguggilla <praneeth.guggilla@okstate.edu>
2017-02-10 12:28:04 -0600

    Update Assignment2.R

--- Assignment2.R
+++ Assignment2.R
@@ -1,14 +1,11 @@
 # RProgramming_assignment2
-
 ## Put comments here that give an overall description of what your
 ## functions do
 ##The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
-
 ##1.set the value of the matrix
 ##2.get the value of the matrix
 ##3.set the value of the matrix
 ##4.get the value of the matrix
-
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -29,8 +26,8 @@ makeCacheMatrix <- function(x = matrix()) {
 # the inverse has already been computed. If so, it gets the result and skips the
 # computation. If not, it computes the inverse, sets the value in the cache via
 # setinverse function.
-
 # This function assumes that the matrix is always invertible. 
+        
 cacheSolve <- function(x, ...) {
   m <- x$getInverse()
   if (!is.null(m)) {
